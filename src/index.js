8e1b4a819cd8ec2869509b78d45fd2ef099362c8
David Vergaray <david.vergaray.a@gmail.com>
2022-01-22 19:25:32 -0300

    Add basic functionality index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 import './style.css';
-import im from './tic-cross.png';
+import crossImage from './tic-cross.png';
+import circleImage from './tic-circle.png';
 
-const ima = new Image();
-ima.src = im;
+const boxes = document.querySelectorAll('.tic-box-container');
 
-document.body.appendChild(ima);
+global.turn = true;
+
+class ticInspector{
+  constructor() {
+    
+  }
+}
+
+const boxIsEmpty = (box) => {
+  if (box.firstElementChild.firstElementChild === null) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+boxes.forEach(box => {
+  box.addEventListener('click', () => {
+    if (boxIsEmpty(box)) {
+      const ticSimbol = new Image();
+      if (turn) {
+        ticSimbol.src = crossImage;
+        turn = false;
+      } else {
+        ticSimbol.src = circleImage;
+        turn = true;
+      }
+      ticSimbol.classList.add('w-100');
+      box.children[0].appendChild(ticSimbol);  
+    }
+  })
+});
+
